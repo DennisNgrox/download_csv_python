7b5448acb0a43ed8ea9b3a907e8cdd658d2b80b1
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-08-19 19:13:21 -0300

    Update app.py
    Retirado Keys e Services que não estão sendo utilizados

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 # Definindo opções de desativar o prompt de Download
